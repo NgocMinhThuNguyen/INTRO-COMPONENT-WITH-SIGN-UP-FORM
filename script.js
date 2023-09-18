48e9275bebb4a8ef6b55f4d0f2e6eb5bb4f06a78
NgocMinhThuNguyen <94945964+NgocMinhThuNguyen@users.noreply.github.com>
2023-09-18 13:16:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,55 @@
-const firstName = document.querySelector("#first-name").value;
-const lastName = document.querySelector("#last-name");
-const email = document.querySelector("#email");
-const password = document.querySelector("#password");
-let errorText = document.querySelectorAll("form .error-text");
-const submitBtn = document.querySelector("button");
-const form = document.querySelector("form");
-const formfs= document.querySelectorAll("form input");
+const form = document.querySelector('form');
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const userInput = document.querySelectorAll('input');
 
-form.addEventListener("submit", (event) => {
-  // event.preventDefault();
-  for (error of errorText) {
-    console.log(error);
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  validateForm();
+  if(validateForm()) {
+    console.log("fsfsfs");
   }
-  checkFirstName(firstName);
-  checkLastName(lastName);
 });
 
-for (element of form) {
-  element.addEventListener('click', (event) => {
-    errorText.classList.remove("show-error")
-  })
+function validateForm() {
+  for (const input of userInput) {
+    let error = input.nextElementSibling;
+    if (input.value === '') {
+      if (input === firstName) {
+        error.textContent = "First Name cannot be blank";
+      } else if (input === lastName) {
+        error.textContent = "Last Name cannot be blank";
+      } else if (input === password) {
+        error.textContent = "Password cannot be blank";
+      } else {
+        error.textContent = "Email cannot be blank"
+      }
+      showError(input, error);
+    } else if ((input.value !== '') && (input === email)) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!(emailRegex.test(input.value))) {
+          error.textContent = "Looks like this is not an email"
+          showError(input, error);
+        } else {
+          showSucces(input, error);
+        }
+    } else {
+      showSucces(input, error);
+    }
+  }
 }
 
-function checkFirstName(firstName) {
-  if (firstName === "") {
-    errorText.textContent = "First Name cannot be empty"
-    errorText.classList.add("show-error");
-  }
-};
+function showError(input, error) {
+  input.classList.add("show-error");
+  error.classList.add("show-error");
+}
 
-function checkFirstName(lastName) {
-  if (lastName === "") {
-    errorText.textContent = "Last Name cannot be empty"
-    errorText.classList.add("show-error");
-  }
-};
+function showSucces(input, error) {
+  input.classList.remove("show-error");
+  error.classList.remove("show-error");
+}
+
